7476a6df447ceef68eb4f49f121aa9d95badfde3
Amit Lisha <u8741936@bsmch.net>
2021-04-20 14:31:40 +0300

    handle new todo and save it in state

--- src/components/TodosCard.tsx
+++ src/components/TodosCard.tsx
@@ -1,17 +1,31 @@
 import React, { FunctionComponent, useState } from "react";
-import { sizing } from "@material-ui/system";
+import TodoInput from "./TodoInput";
 import Paper from "@material-ui/core/Paper";
 import { Box } from "@material-ui/core";
+import TodoItem from "./TodoItem";
+import Todo from "../models/Todo";
 
 interface IProps {}
 
 const TodosCard: FunctionComponent<IProps> = () => {
-  const [isDone, setIsDone] = useState(false);
+  const [todos, setTodos] = useState<Array<Todo>>([]);
+
+  const handleTodoSubmit = (todoText: string) => {
+    setTodos((oldArray) => [
+      ...oldArray,
+      new Todo({ text: todoText, isCompleted: false }),
+    ]);
+  };
 
   return (
     <div>
-      <Box m="auto" width="30%" height="30%">
-        <Paper>Amit</Paper>
+      <Box m="auto" width="30%">
+        <Paper>
+          <TodoInput onSubmit={handleTodoSubmit}></TodoInput>
+          {todos.map((todo) => (
+            <TodoItem todoText={todo.text}></TodoItem>
+          ))}
+        </Paper>
       </Box>
     </div>
   );
