2c9a15fee36e9ebf7914433055c9f28705b38e1e
Amit Lisha <u8741936@bsmch.net>
2021-04-20 15:06:27 +0300

    get a todo object as a prop instead of just text, a delete button also added and an event prop

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,23 +1,32 @@
 import Checkbox from "@material-ui/core/Checkbox";
+import IconButton from "@material-ui/core/IconButton";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
 import ListItemText from "@material-ui/core/ListItemText";
+import DeleteForeverIcon from "@material-ui/icons/DeleteForever";
 import React, { FunctionComponent, useState } from "react";
+import Todo from "../models/Todo";
 
 interface IProps {
-  todoText: string;
+  todo: Todo;
+  onDelete: (todoToDelete: Todo) => void;
 }
 
-const TodoItem: FunctionComponent<IProps> = ({ todoText }) => {
+const TodoItem: FunctionComponent<IProps> = ({ todo, onDelete }) => {
   const [isDone, setIsDone] = useState(false);
 
   const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setIsDone(event.target.checked);
   };
 
+  const handleTodoDelete = () => {
+    onDelete(todo);
+  };
+
   return (
     <div>
-      <ListItem dense button>
+      <ListItem dense button divider>
         <ListItemIcon>
           <Checkbox
             edge="start"
@@ -29,8 +38,13 @@ const TodoItem: FunctionComponent<IProps> = ({ todoText }) => {
         </ListItemIcon>
         <ListItemText
           style={{ textDecoration: isDone ? "line-through" : "none" }}
-          primary={todoText}
+          primary={todo.text}
         />
+        <ListItemSecondaryAction>
+          <IconButton edge="end" aria-label="delete" onClick={handleTodoDelete}>
+            <DeleteForeverIcon />
+          </IconButton>
+        </ListItemSecondaryAction>
       </ListItem>
     </div>
   );
