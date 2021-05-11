2a81e1ed39b5079e77476022f5045b54c940997d
Artemis <artemischips@gmail.com>
2021-05-11 10:58:27 +0300

    Todo.ts + TodoItem.css comments

--- src/models/Todo.ts
+++ src/models/Todo.ts
@@ -1,9 +1,15 @@
+// TODO: optional properties should come last and shouldn't be mixed with mandatory properties
 export default class Todo {
+  // TODO: how can id be optional?
   id?: number;
   text!: string;
+  // TODO: why is it mandatory? fields like this should have default value.
+  // think about the case when you create a new todo, it is not completed by default
   isCompleted!: boolean;
   deadlineTime?: Date;
 
+  // TODO: it seems like you don't create an instance using this ctor.
+  // if you don't use it, interface is better because typescript can import it as type only and not value which will help typescript engine with optimiazation process
   constructor(todo: Todo) {
     Object.assign(this, todo);
   }
